79c2ad5569a7d0720d3908c58546b2b4b3380b0e
datnt <datnt@codluck.com>
2023-02-07 11:51:51 +0700

    update

--- src/main/java/me/sntrz/mythicaddon/Drop/DropSlimefunItem.java
+++ src/main/java/me/sntrz/mythicaddon/Drop/DropSlimefunItem.java
@@ -6,8 +6,11 @@ import io.lumine.mythic.api.config.MythicLineConfig;
 import io.lumine.mythic.api.drops.DropMetadata;
 import io.lumine.mythic.api.drops.IItemDrop;
 import io.lumine.mythic.bukkit.adapters.BukkitItemStack;
+import io.lumine.mythic.bukkit.utils.logging.Log;
+import io.lumine.mythic.core.drops.droppables.ItemDrop;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 
 public class DropSlimefunItem implements IItemDrop {
@@ -16,7 +19,13 @@ public class DropSlimefunItem implements IItemDrop {
 
     public void prepareItem(String item_id, Integer quantity) {
         SlimefunItem slime_item = SlimefunItem.getById(item_id);
-        if (!slime_item.equals(null)) {
+        Log.info(item_id);
+        if (slime_item == null) {
+            Log.info("Item = null");
+        } else {
+            Log.info("Item != null");
+        }
+        if (slime_item != null) {
             this.drop_item = slime_item.getItem();
             this.drop_item.setAmount(quantity);
         }
@@ -25,17 +34,15 @@ public class DropSlimefunItem implements IItemDrop {
     public DropSlimefunItem(MythicLineConfig config, String argument) {
         String item_id = config.getString(new String[]{"id", "i"}, "COPPER_DUST", argument);
         Integer quantity = config.getInteger(new String[]{"amount", "a"}, 0);
-
         prepareItem(item_id.toUpperCase(), quantity);
-
-    }
-
-    public BukkitItemStack getDrop_item() {
-        return new BukkitItemStack(this.drop_item);
     }
 
     @Override
     public AbstractItemStack getDrop(DropMetadata dropMetadata, double v) {
+        ItemMeta meta = this.drop_item.getItemMeta();
+        if (meta != null && meta.hasDisplayName()) {
+            Log.info(meta.getDisplayName());
+        }
         return new BukkitItemStack(this.drop_item);
     }
 }
--- src/main/java/me/sntrz/mythicaddon/MythicAddon.java
+++ src/main/java/me/sntrz/mythicaddon/MythicAddon.java
@@ -1,12 +1,14 @@
 package me.sntrz.mythicaddon;
 
 import io.lumine.mythic.api.config.MythicLineConfig;
+import io.lumine.mythic.api.drops.IDrop;
 import io.lumine.mythic.bukkit.adapters.BukkitItemStack;
 import io.lumine.mythic.bukkit.events.MythicDropLoadEvent;
 import io.lumine.mythic.bukkit.utils.logging.Log;
 import io.lumine.mythic.core.drops.droppables.CustomDrop;
 import io.lumine.mythic.core.drops.droppables.ItemDrop;
 import me.sntrz.mythicaddon.Drop.DropSlimefunItem;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -18,7 +20,7 @@ public final class MythicAddon extends JavaPlugin implements Listener {
     public void onEnable() {
         // Plugin startup logic
         Log.info("MythicDrop startup-ing");
-
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @Override
@@ -30,16 +32,9 @@ public final class MythicAddon extends JavaPlugin implements Listener {
     public void onMythicDropLoad(MythicDropLoadEvent event) {
         Log.info("MythicDropLoadEvent called for drop " + event.getDropName());
 
-        if (event.getDropName().equalsIgnoreCase("SLIMEFUN")) {
-            String line = event.getConfig().getLine();
-            MythicLineConfig config = event.getConfig();
-
-            BukkitItemStack item_drop = new DropSlimefunItem(event.getConfig(), event.getArgument()).getDrop_item();
-
-            ItemDrop drop = new ItemDrop(line, config, item_drop);
-            event.register(drop);
-
-            Log.info("-- Registered slimefun drop!");
+        if(event.getDropName().equalsIgnoreCase("slimefun"))	{
+            event.register(new DropSlimefunItem(event.getConfig(), event.getArgument()));
+            Log.info("-- Registered DropSlimefunItem drop!");
         }
     }
 }
